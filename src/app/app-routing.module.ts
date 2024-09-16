9f8bfcb6769c6b3fe21132b655d677550e55ae00
Mauricio Alvarado <mauriciorianoa@gmail.com>
2024-09-16 00:26:31 -0500

    feat: Ajuste de Rutas udistrital/argo_documentacion/issues/123

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {provideRouter, RouterModule, Routes} from '@angular/router';
 import { RegistroPolizaComponent } from './modules/registro-poliza/registro-poliza.component';
+import {provideHttpClient, withFetch} from "@angular/common/http";
+import {getSingleSpaExtraProviders} from "single-spa-angular";
+import {APP_BASE_HREF} from "@angular/common";
 
 const routes: Routes = [
   {
-    path:"registrar-poliza",
+    path:"registrar",
     component: RegistroPolizaComponent
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    provideRouter(routes),
+    { provide: APP_BASE_HREF, useValue: '/polizas/' },
+    getSingleSpaExtraProviders(),
+    provideHttpClient(withFetch()),
+  ],
 })
 export class AppRoutingModule { }
